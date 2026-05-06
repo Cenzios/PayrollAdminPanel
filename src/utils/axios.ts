eed25ac1cf2775a27ef7f74840ed3d75684da5f1
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2026-05-06 14:58:42 +0530

    update axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://payrolladminproductionserver.cenzios.com/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 console.log('🔌 API BASE URL:', BASE_URL);
 
