b6fc455a8f9793b14d0224cd8672c693c3224e56
Hasini Bandara <dmhnbandara@gmail.com>
2026-05-13 14:41:23 +0530

    button update

--- src/pages/ManualPayments.tsx
+++ src/pages/ManualPayments.tsx
@@ -221,7 +221,7 @@ export default function ManualPayments() {
                                                                     }}
                                                                     className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 flex items-center gap-2"
                                                                 >
-                                                                    <X size={16} /> Reject Payment !!!
+                                                                    <X size={16} /> Reject Payment
                                                                 </button>
                                                             </>
                                                         ) : (
